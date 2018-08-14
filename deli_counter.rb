b2be4c6eecf32c1d6d59eac5868725f860ac5ee0
Katrina Pritchard <pritckat@gmail.com>
2018-08-14 21:46:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ def now_serving(array)
   if array.length == 0
     puts "There is nobody waiting to be served!"
   else
-    array.shift
+    #array.shift
     puts "Currently serving #{array.shift}."
   end
 end
