60226bdcf6c4d258f10bb269f95180ab09460a72
Katrina Pritchard <pritckat@gmail.com>
2018-08-14 21:34:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,11 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   else
-    puts array.length
+    line = []
+    array.each_with_index do |person, place|
+      "#{place}. #{person}" << line
+    end
+    line = line.join(" ")
+    puts "The line is currently: " + line
   end
 end
